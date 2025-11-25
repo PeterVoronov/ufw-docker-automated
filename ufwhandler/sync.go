31abf1c75684390148d924ef9517a50a6c2f344d
Peter Voronov <peter.voronov@gmail.com>
2025-11-25 23:14:39 +0100

    Update packages and code to make it compatible with at least version is 1.44 of docker API

--- ufwhandler/sync.go
+++ ufwhandler/sync.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/rs/zerolog/log"
 )
@@ -13,7 +14,7 @@ func Sync(ctx *context.Context, createChannel chan *types.ContainerJSON, client 
 	// Returns only running containers
 	filter := filters.NewArgs()
 	filter.Add("label", "UFW_MANAGED=TRUE")
-	containers, err := client.ContainerList(*ctx, types.ContainerListOptions{Filters: filter})
+	containers, err := client.ContainerList(*ctx, container.ListOptions{Filters: filter})
 	if err != nil {
 		log.Error().Err(err).Msg("ufw-docker-automated: Couldn't retrieve existing containers.")
 	}
